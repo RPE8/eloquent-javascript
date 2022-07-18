b9f99fb90669a04fdf7825ae0c539d5024c5ff06
RPE <vitali.shuman96@gmail.com>
2022-07-18 15:34:58 +0300

    Add solution for Recursion with tests

---
+++ 03_functions/Recursion.js
@@ -0,0 +1,17 @@
+function isEven(number) {
+  if (number < 0) {
+    number = Math.abs(number);
+  }
+
+  if (number === 0) {
+    return true;
+  }
+
+  if (number === 1) {
+    return false;
+  }
+
+  return isEven(number - 2);
+}
+
+module.exports = { isEven };
--- test/03_functions.js
+++ test/03_functions.js
@@ -1,4 +1,5 @@
 const { min } = require("../03_functions/Minimum.js");
+const { isEven } = require("../03_functions/Recursion.js");
 
 const assert = require("assert");
 
@@ -17,5 +18,29 @@ describe("Minimum", function () {
   });
   it("min(1,-1)", function () {
     assert.equal(min(1, -1), -1);
+  });
+});
+
+describe("Recursion", function () {
+  it("isEven(0)", function () {
+    assert.equal(isEven(0), true);
+  });
+  it("isEven(1)", function () {
+    assert.equal(isEven(1), false);
+  });
+  it("isEven(50)", function () {
+    assert.equal(isEven(50), true);
+  });
+  it("isEven(75)", function () {
+    assert.equal(isEven(75), false);
+  });
+  it("isEven(-1)", function () {
+    assert.equal(isEven(-1), false);
+  });
+  it("isEven(-50)", function () {
+    assert.equal(isEven(-50), true);
+  });
+  it("isEven(-75)", function () {
+    assert.equal(isEven(-75), false);
   });
 });
