130b3e1c4247bd6d6c06748aba7c03875014f429
RPE <vitali.shuman96@gmail.com>
2022-07-19 17:43:47 +0300

    Add solutionnfor The locked box

---
+++ 08_error/The-locked-box.js
@@ -0,0 +1,43 @@
+const box = {
+  locked: true,
+  unlock() {
+    this.locked = false;
+  },
+  lock() {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  },
+};
+
+function withBoxUnlocked(body) {
+  let wasLocked = false;
+  if (box.locked) {
+    wasLocked = true;
+    box.unlock();
+  }
+
+  try {
+    body(box.content);
+  } catch (e) {
+  } finally {
+    if (wasLocked) box.lock();
+  }
+}
+
+withBoxUnlocked(function () {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function () {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e);
+}
+console.log(box.locked);
+// → true
