6dc7c57e9e447c15957518457c04a4ab18fef6f7
RPE <vitali.shuman96@gmail.com>
2022-07-19 15:00:39 +0300

    Add solution for Group and tests

---
+++ 06_object/Groups.js
@@ -0,0 +1,54 @@
+class Group {
+  #group;
+  constructor() {
+    this.#group = Object.create(null);
+  }
+
+  #getGroup() {
+    return this.#group;
+  }
+
+  #setGroup(value) {
+    this.#group = value;
+  }
+
+  add(value) {
+    const group = this.#getGroup();
+
+    if (this.has(value)) {
+      return;
+    }
+
+    group[value] = true;
+  }
+
+  delete(value) {
+    const group = this.#getGroup();
+
+    if (!this.has(value)) {
+      return;
+    }
+
+    delete group[value];
+  }
+
+  has(value) {
+    const group = this.#getGroup();
+
+    return value in group;
+  }
+
+  static from(iterable) {
+    if (!iterable[Symbol.iterator]) {
+      return undefined;
+    }
+
+    let newGroup = new Group();
+    for (const value of iterable) {
+      newGroup.add(value);
+    }
+    return newGroup;
+  }
+}
+
+module.exports = Group;
--- test/06_object.js
+++ test/06_object.js
@@ -3,6 +3,7 @@ const should = chai.should();
 const expect = chai.expect;
 
 const Vector = require("../06_object/A-vector-type.js");
+const Group = require("../06_object/Groups.js");
 
 describe("Vector", () => {
   it("Vector should be created succesfully", () => {
@@ -39,7 +40,25 @@ describe("Vector", () => {
 
     vector.length.should.equal(5);
   });
+});
 
-  // vector.getX().should.equal(1);
-  // vector.getY().should.equal(2);
+describe("Group", () => {
+  it("Group should be created succesfully", () => {
+    const group = new Group();
+    (group instanceof Group).should.equal(true);
+  });
+  it("Group from  & has methods should work correctly", () => {
+    const group = Group.from([10, 20]);
+    group.has(10).should.equal(true);
+    group.has(20).should.equal(true);
+    group.has(30).should.equal(false);
+    group.add(30);
+    group.has(30).should.equal(true);
+    group.has(10).should.equal(true);
+    group.has(20).should.equal(true);
+    group.delete(10);
+    group.has(30).should.equal(true);
+    group.has(10).should.equal(false);
+    group.has(20).should.equal(true);
+  });
 });
